d3eab525f186e1ed5ed8586c1f95edb8de468715
Stephanie Norton <steph.jane.norton@gmail.com>
2021-02-08 09:07:10 -0700

    Update subscribe section design w. columns & image

--- src/components/SubscribeContainer.js
+++ src/components/SubscribeContainer.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import photo from '../assets/Fin-Rah-Zel-Titled-Pic.png'
 
 const SubscribeContainer = () => {
   const [email, setEmail] = useState('')
@@ -13,19 +14,24 @@ const SubscribeContainer = () => {
   return (
     <>
       <section className="subscribe-container">
-        <h2 className="section-heading">Subscribe</h2>
-        {!subscribed &&
-          <>
-            <p>No FOMO here! Subscribe to keep up to date </p>
-            <div className="form-area">
-              <input id="emailInput" name="email" type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-              <button id="submit-btn" onClick={() => handleClick()}>SUBMIT</button>
-            </div>
-          </>
-        }
-        {subscribed &&
-          <p>Thanks for signing up!</p>
-        }
+        <section className="column-1">
+        <img className="img" src={photo} alt="Fin Rah Zel holding guitar"/>
+        </section>
+        <section className="column-2">
+          <h2 className="section-heading">Subscribe</h2>
+          {!subscribed &&
+            <>
+              <p>No FOMO here! Subscribe to keep up to date </p>
+              <div className="form-area">
+                <input id="emailInput" name="email" type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <button id="submit-btn" onClick={() => handleClick()}>SUBMIT</button>
+              </div>
+            </>
+          }
+          {subscribed &&
+            <p>Thanks for signing up!</p>
+          }
+        </section>
       </section>
     </>
   )
