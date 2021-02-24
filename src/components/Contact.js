74067c318bf6f44b1abbb55e0b88474e2ac1c111
Stephanie Norton <steph.jane.norton@gmail.com>
2021-02-24 09:29:19 -0700

    Change image in contact section and section layout

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import emailjs from 'emailjs-com'
-import photo from '../assets/Fin-Rah-Zel-Titled-Pic.png'
+import photo from '../assets/nature-background.jpg'
 
 const Contact = () => {
   const [ name, setName ] = useState('')
@@ -32,31 +32,34 @@ const Contact = () => {
   return (
     <>
       <section className="contact">
-        <section className="column-1">
-        <img className="img" src={photo} alt="Fin Rah Zel holding guitar"/>
-        </section>
-        <section className="column-2">
-          <div className="sec-title-container">
+        {/* <section className="column-1"> */}
+        <img className="contact-img" src={photo} alt="Fin Rah Zel holding guitar"/>
+        {/* </section> */}
+        {/* <section className="column-2"> */}
+        <div className="form-area">
+          {/* <div className="sec-title-container"> */}
             <h2 className="section-heading">CONTACT</h2>
-          </div>
+        {/* </div> */}
+
           {(!msgSubmitted && !msgError) &&
             <>
               <p>Send a message to Fin Rah Zel</p>
-              <div className="form-area">
+      
                 <input id="nameInput" name="name" type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
                 <input id="emailInput" name="email" type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-              <textarea id="message" name="message" type="text" placeholder="Your Message..." rows="8" maxlength='250' value={message} onChange={(e) => setMessage(e.target.value)} />
+              <textarea id="message" name="message" type="text" placeholder="Your Message..." rows="5" maxLength='250' value={message} onChange={(e) => setMessage(e.target.value)} />
                 <button id="submit-btn" onClick={() => handleClick()}>SEND</button>
-              </div>
+             
             </>
           }
+        </div>
           {msgSubmitted &&
             <p>Thanks for your message!</p>
           }
           {msgError &&
             <p>Sorry, something went wrong and we couldn't send your message. Please refresh the page and try again.</p>
           }
-        </section>
+        {/* </section> */}
       </section>
     </>
   )
