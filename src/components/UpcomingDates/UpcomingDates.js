49dccc83e0b9a05dc23d1d5cd7c85ed36813a6b5
Stephanie Norton <steph.jane.norton@gmail.com>
2021-02-27 15:30:05 -0700

    Fix centering issues

--- src/components/UpcomingDates/UpcomingDates.js
+++ src/components/UpcomingDates/UpcomingDates.js
@@ -7,7 +7,7 @@ import './UpcomingDates.css'
 const UpcomingDates = () => {
   return (
     <section className="upcoming-gigs section">
-      <h2 className="h2">Upcoming Gigs</h2>
+      <h2 className="h2 upcoming-h2">Upcoming Gigs</h2>
       <ul className="event-list">
         <h3 className="h3">March</h3>
         <li className="event">Barry's bar, Saturday October 15th 2021 @9am</li>
