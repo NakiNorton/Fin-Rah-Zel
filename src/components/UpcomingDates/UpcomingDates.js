49d6d8d5791b18cd6a4aa0d60a7848f947a2bdb2
Stephanie Norton <steph.jane.norton@gmail.com>
2021-02-27 10:21:50 -0700

    Remove makeStyles import and make avatar responsive

--- src/components/UpcomingDates/UpcomingDates.js
+++ src/components/UpcomingDates/UpcomingDates.js
@@ -1,26 +1,10 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
 import artistPhoto1 from '../../assets/musician-1.jpg'
 import Avatar from '@material-ui/core/Avatar';
 
 import './UpcomingDates.css'
 
 const UpcomingDates = () => {
-
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      width: '300px',
-      height: '300px',
-      opacity: '0.9',
-      margin: '0 auto',
-      '& > *': {
-        margin: theme.spacing(1),
-      },
-    },
-  }))
-
-  const classes = useStyles()
-
   return (
     <section className="upcoming-gigs section">
       <h2 className="h2">Upcoming Gigs</h2>
@@ -32,8 +16,8 @@ const UpcomingDates = () => {
         <li className="event">Bri bar, Saturday October 15th 2021 @9am</li>
       </ul>
 
-      <div className="avatar-img">
-        <Avatar className={classes.root} src={artistPhoto1} alt="Fin Rah Zel playing guitar" />
+      <div className="avatar-container">
+        <Avatar id="avatar-img" src={artistPhoto1} alt="Fin Rah Zel playing guitar" />
       </div>
     </section>
   )
