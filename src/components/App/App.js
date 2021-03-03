7213267b449722f23649fe395bc99090b55dca06
Stephanie Norton <steph.jane.norton@gmail.com>
2021-03-02 19:15:07 -0700

    Revert to original structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,18 @@
 import React, { lazy, Suspense } from 'react'
-// import FadeInSection from '../UI_Animations/FadeInAnimation'
+import FadeInSection from '../UI_Animations/FadeInAnimation'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import './App.css';
 import './AppMQ.css';
 import LoadingSpinner from '../UI_Animations/LoadingSpinner'
-
 import LandingPage from '../LandingPage/LandingPage'
-// const About = lazy(() => import('../About/About'))
-const NavBar = lazy(() => import('../NavBar/NavBar'))
-// const MusicContainer = lazy(() => import('../MusicContainer/MusicContainer'))
-// const UpcomingDates = lazy(() => import('../UpcomingDates/UpcomingDates'))
-// const Contact = lazy(() => import('../Contact/Contact'))
+import NavBar from '../NavBar/NavBar'
+
+
+const About = lazy(() => import('../About/About'))
+const MusicContainer = lazy(() => import('../MusicContainer/MusicContainer'))
+const UpcomingDates = lazy(() => import('../UpcomingDates/UpcomingDates'))
+const Contact = lazy(() => import('../Contact/Contact'))
 
 library.add(fab)
 
@@ -20,11 +21,24 @@ const App = () => {
   return (
     <div className="App">
       <Suspense fallback={<LoadingSpinner />}>
-    
         <NavBar />
         <LandingPage id='landing-page'/>
 
-      
+        <FadeInSection>
+          <About id='about' />
+        </FadeInSection>
+
+        <FadeInSection>
+          <MusicContainer id='music' />
+        </FadeInSection>
+
+        <FadeInSection>
+          <UpcomingDates id='upcoming-gigs' />
+        </FadeInSection>
+
+        <FadeInSection>
+          <Contact id='contact' />
+        </FadeInSection > 
 
         <footer>
           <div className="footer">
