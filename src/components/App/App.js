0f1166534d0f66484c64d5e957531e05f182612f
Stephanie Norton <steph.jane.norton@gmail.com>
2021-03-02 18:49:35 -0700

    Move landing page div to be rendered in App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,22 @@
 import React, { lazy, Suspense } from 'react'
-// import FadeInSection from '../UI_Animations/FadeInAnimation'
+import FadeInSection from '../UI_Animations/FadeInAnimation'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import './App.css';
 import './AppMQ.css';
 import LoadingSpinner from '../UI_Animations/LoadingSpinner'
+// import LandingPage from '../LandingPage/LandingPage'
+import NavBar from '../NavBar/NavBar'
+import backgroundPhoto from '../../assets/FinRahZel-9.svg'
+import '..//LandingPage/LandingPage.css'
 
-import LandingPage from '../LandingPage/LandingPage'
-// const About = lazy(() => import('../About/About'))
-const NavBar = lazy(() => import('../NavBar/NavBar'))
-// const MusicContainer = lazy(() => import('../MusicContainer/MusicContainer'))
-// const UpcomingDates = lazy(() => import('../UpcomingDates/UpcomingDates'))
-// const Contact = lazy(() => import('../Contact/Contact'))
+
+const About = lazy(() => import('../About/About'))
+const MusicContainer = lazy(() => import('../MusicContainer/MusicContainer'))
+const UpcomingDates = lazy(() => import('../UpcomingDates/UpcomingDates'))
+const Contact = lazy(() => import('../Contact/Contact'))
+
+
 
 library.add(fab)
 
@@ -20,11 +25,29 @@ const App = () => {
   return (
     <div className="App">
       <Suspense fallback={<LoadingSpinner />}>
-    
         <NavBar />
-        <LandingPage id='landing-page'/>
+        <div className="landing-page">
+          <header className="header">
+            <h1 className="header-text">Fin Rah Zel</h1>
+            <img className="header-background-img" src={backgroundPhoto} alt="Fin Rah Zel playing guitar" />
+          </header>
+        </div>
 
-      
+        <FadeInSection>
+          <About id='about' />
+        </FadeInSection>
+
+        <FadeInSection>
+          <MusicContainer id='music' />
+        </FadeInSection>
+
+        <FadeInSection>
+          <UpcomingDates id='upcoming-gigs' />
+        </FadeInSection>
+
+        <FadeInSection>
+          <Contact id='contact' />
+        </FadeInSection > 
 
         <footer>
           <div className="footer">
