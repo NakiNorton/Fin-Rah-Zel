79278ab3a25c008f66938c93a48c38c2aa35b234
Stephanie Norton <steph.jane.norton@gmail.com>
2021-03-02 18:21:42 -0700

    Center spinner

--- src/components/UI_Animations/LoadingSpinner.js
+++ src/components/UI_Animations/LoadingSpinner.js
@@ -1,22 +1,11 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    '& > * + *': {
-      marginLeft: theme.spacing(2),
-    },
-  },
-}));
 
 export default function LoadingSpinner() {
-  const classes = useStyles();
-
   return (
-    <div className={classes.root}>
+    <div style={{ height: '200px', marginTop: '15%' }}>
       <CircularProgress />
-    </div>
+    </div >
   )
 }
