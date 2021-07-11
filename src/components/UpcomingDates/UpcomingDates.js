4615675c5ca902733dd3acfeb01db8e686a8875e
Stephanie Norton <steph.jane.norton@gmail.com>
2021-07-10 18:08:57 -0600

    Update upcoming dates styles

--- src/components/UpcomingDates/UpcomingDates.js
+++ src/components/UpcomingDates/UpcomingDates.js
@@ -4,11 +4,11 @@ import './UpcomingDates.css'
 const UpcomingDates = () => {
   return (
     <section className="upcoming-gigs section">
-      <h2 className="h2 upcoming-h2">Upcoming Gigs</h2>
+      <h2 className="h2 upcoming-h2" style={{ paddingTop: '5rem' }}>Upcoming Gigs</h2>
       <ul className="event-list">
         <h3 className="h3">Friday 16 July 2021</h3>
         <li className="event">Support act for Sons Of Zion, The Square, Palmerston North</li>
-        <h3 className="h3">Details TBA</h3>
+        <h3 className="h3">TBA</h3>
         <li className="event">"Power" Single release show</li>
       </ul>
     </section>
